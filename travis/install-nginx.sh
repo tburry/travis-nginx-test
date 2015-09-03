d4f3db46f70499dc95f66060c20b82fe824e1722
Todd Burry <todd@vanillaforums.com>
2015-09-03 17:21:58 -0400

    Skeleton nginx install script

---
+++ travis/install-nginx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -x
+
+USER=$(whoami)
+DIR=$(realpath $(dirname "$0"))
+TRAVIS_PHP_VERSION=$(phpenv version-name)
+#DOCUMENT_ROOT=$(realpath "$DIR/../tests")
+PORT=9000
+SERVER="127.0.0.1:$PORT"
+
+mkdir "$DIR/nginx"
